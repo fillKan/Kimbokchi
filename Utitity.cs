113e7e6a889d66ea14ffc208c22336a8a02be0e4
한대건 <han15050424@gmail.com>
2020-09-27 19:04:50 +0900

    각종 유용한 기능들을 담는 클래스 Utility클래스를 추가하며, Swap기능을 구현한다
    앞으로 Utility클래스에는 유용한 여러 기능들이 추가될 것이다.
    현재 추가된 기능은 두 값을 스왑하는 기능이다.

---
+++ Utitity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kimbokchi
+{
+    public static class Utitity
+    {
+        public static void Swap<T>(T a, T b)
+        {
+            T temp = a; a = b; b = temp;
+        }
+    }
+}
