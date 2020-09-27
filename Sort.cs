66e1363995cf8a46770ac911784d4e1790e41fbc
한대건 <han15050424@gmail.com>
2020-09-27 13:46:35 +0900

    List 컬렉션의 병합정렬을 구현하고, 이들을 확장 메서드로 사용할 수 있게끔 한다
    구현한 List의 병합정렬 또한 배열의 병합정렬과 마찬가지로 범위를 지정하여 정렬을 실행하는 함수와
    모든 요소들을 정렬하는 함수로 구분하였다.

--- Sort.cs
+++ Sort.cs
@@ -6,12 +6,12 @@ namespace Kimbokchi
 {
     public static class Sort
     {
-        public static void MergeSort<T>(T[] array, Func<T, T, bool> IsRValueBigger)
+        public static void MergeSort<T>(this T[] array, Func<T, T, bool> IsRValueBigger)
         {
             MergeSort(array, 0, array.Length - 1, new T[array.Length], IsRValueBigger);
         }
 
-        public static void MergeSort<T>(T[] array, int min, int max, T[] tempArray, Func<T, T, bool> IsRValueBigger)
+        public static void MergeSort<T>(this T[] array, int min, int max, T[] tempArray, Func<T, T, bool> IsRValueBigger)
         {
             if (min >= max) return;
 
@@ -43,6 +43,45 @@ namespace Kimbokchi
                 }
             }
             for (i = min; i <= max; i++) array[i] = tempArray[i];
+        }
+
+        public static void MergeSort<T>(this List<T> list, Func<T, T, bool> IsRValueBigger)
+        {
+            MergeSort(list, 0, list.Count - 1, new List<T>(), IsRValueBigger);
+        }
+
+        public static void MergeSort<T>(this List<T> list, int min, int max, List<T> tempList, Func<T, T, bool> IsRValueBigger)
+        {
+            if (min >= max) return;
+
+            int mid = (min + max) / 2;
+
+            MergeSort(list, min,     mid, tempList, IsRValueBigger);
+            MergeSort(list, mid + 1, max, tempList, IsRValueBigger);
+
+            int i = min;
+            int j = mid + 1;
+
+            for (int k = min; k <= max; k++)
+            {
+                if (i > mid)
+                {
+                    tempList[k] = list[j++];
+                }
+                else if (j > max)
+                {
+                    tempList[k] = list[i++];
+                }
+                else if (IsRValueBigger(list[i], list[j]))
+                {
+                    tempList[k] = list[i++];
+                }
+                else
+                {
+                    tempList[k] = list[j++];
+                }
+            }
+            for (i = min; i <= max; i++) list[i] = tempList[i];
         }
     }
 }
